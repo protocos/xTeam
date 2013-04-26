ae33adf930919e10f9f0ea4a11a08efa64497b19
protocos <protocos1911@gmail.com>
2013-04-26 15:14:39 -0500

    Updated CommandManager

--- xTeam/src/me/protocos/xteam/command/CommandManager.java
+++ xTeam/src/me/protocos/xteam/command/CommandManager.java
@@ -8,10 +8,6 @@ public class CommandManager implements ICommandManager
 	private HashList<String, ICommandUsage> usages;
 	private HashList<String, IPermissionNode> nodes;
 
-	//	private static HashList<String, BaseConsoleCommand> consoleCommands = new HashList<String, BaseConsoleCommand>();
-	//	private static HashList<String, BaseServerAdminCommand> adminCommands = new HashList<String, BaseServerAdminCommand>();;
-	//	private static HashList<String, BaseUserCommand> userCommands = new HashList<String, BaseUserCommand>();
-
 	public CommandManager()
 	{
 		patterns = new HashList<String, ICommandPattern>();
@@ -41,75 +37,4 @@ public class CommandManager implements ICommandManager
 	{
 		return nodes.get(key).getPermissionNode();
 	}
-	//	public static void registerConsoleCommand(String key, BaseConsoleCommand command)
-	//	{
-	//		consoleCommands.put(key, command);
-	//	}
-	//	public static void registerAdminCommand(String key, BaseServerAdminCommand command)
-	//	{
-	//		adminCommands.put(key, command);
-	//	}
-	//	public static void registerUserCommand(String key, BaseUserCommand command)
-	//	{
-	//		userCommands.put(key, command);
-	//	}
-	//	public static String getAdminPattern(String key)
-	//	{
-	//		return StringUtil.OPTIONAL_WHITE_SPACE + adminCommands.get(key).getPattern();
-	//	}
-	//	public static String getAdminPermissionNode(String key)
-	//	{
-	//		return adminCommands.get(key).getPermissionNode();
-	//	}
-	//	public static String getAdminUsage(String key)
-	//	{
-	//		return adminCommands.get(key).getUsage();
-	//	}
-	//	public static String getConsolePattern(String key)
-	//	{
-	//		return StringUtil.OPTIONAL_WHITE_SPACE + consoleCommands.get(key).getPattern();
-	//	}
-	//	public static String getConsoleUsage(String key)
-	//	{
-	//		return consoleCommands.get(key).getUsage();
-	//	}
-	//	public static String getUserPattern(String key)
-	//	{
-	//		return StringUtil.OPTIONAL_WHITE_SPACE + userCommands.get(key).getPattern();
-	//	}
-	//	public static String getUserPermissionNode(String key)
-	//	{
-	//		return userCommands.get(key).getPermissionNode();
-	//	}
-	//	public static String getUserUsage(String key)
-	//	{
-	//		return userCommands.get(key).getUsage();
-	//	}
-	//	public static boolean matchesAdminCmd(String command)
-	//	{
-	//		for (int i = 0; i < adminCommands.size(); i++)
-	//		{
-	//			if (command.matches(adminCommands.get(i).getPattern()))
-	//				return true;
-	//		}
-	//		return false;
-	//	}
-	//	public static boolean matchesConsoleCmd(String command)
-	//	{
-	//		for (int i = 0; i < consoleCommands.size(); i++)
-	//		{
-	//			if (command.matches(consoleCommands.get(i).getPattern()))
-	//				return true;
-	//		}
-	//		return false;
-	//	}
-	//	public static boolean matchesUserCmd(String command)
-	//	{
-	//		for (int i = 0; i < userCommands.size(); i++)
-	//		{
-	//			if (command.matches(userCommands.get(i).getPattern()))
-	//				return true;
-	//		}
-	//		return false;
-	//	}
 }
