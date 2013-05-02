647b47bd921c6d6153f98c827d534c8b474ab467
protocos <protocos1911@gmail.com>
2013-05-02 16:04:56 -0500

    Fixed the stupid parseLong( bug

--- xTeam/src/me/protocos/xteam/core/Team.java
+++ xTeam/src/me/protocos/xteam/core/Team.java
@@ -257,7 +257,7 @@ public class Team
 			String tag = teamProperties.get("tag") != null ? teamProperties.get("tag") : name;
 			boolean openJoining = Boolean.parseBoolean(teamProperties.get("open") != null ? teamProperties.get("open") : "false");
 			boolean defaultTeam = Boolean.parseBoolean(teamProperties.get("default") != null ? teamProperties.get("default") : "false");
-			long timeHeadquartersSet = Long.parseLong(teamProperties.get("timeHeadquartersSet") != null ? teamProperties.get("timeHeadquartersSet") : "0L");
+			long timeHeadquartersSet = Long.parseLong(teamProperties.get("timeHeadquartersSet") != null ? teamProperties.get("timeHeadquartersSet") : "0");
 			String hq = teamProperties.get("Headquarters") != null ? teamProperties.get("Headquarters") : (hq = teamProperties.get("hq") != null ? teamProperties.get("hq") : "");
 			if (teamProperties.containsKey("world"))
 				hq = teamProperties.get("world") + "," + hq;
