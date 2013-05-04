08ea9d12378d7c625dd2b6ec9a29ffb7719a84b2
protocos <protocos1911@gmail.com>
2013-05-04 12:14:32 -0500

    Included LimitedQueue with logging

---
+++ xTeam/src/me/protocos/xteam/util/LimitedQueue.java
@@ -0,0 +1,60 @@
+package me.protocos.xteam.util;
+
+import java.util.AbstractQueue;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LimitedQueue<T> extends AbstractQueue<T>
+{
+	private Queue<T> data;
+	private int maxSize;
+
+	public LimitedQueue(int maxSize)
+	{
+		data = new LinkedList<T>();
+		this.maxSize = maxSize;
+	}
+
+	@Override
+	public boolean offer(T element)
+	{
+		if (data.size() >= maxSize)
+			data.poll();
+		return data.offer(element);
+	}
+
+	@Override
+	public T peek()
+	{
+		return data.peek();
+	}
+
+	@Override
+	public T poll()
+	{
+		return data.poll();
+	}
+
+	@Override
+	public Iterator<T> iterator()
+	{
+		return data.iterator();
+	}
+
+	@Override
+	public int size()
+	{
+		return maxSize;
+	}
+
+	public String toString()
+	{
+		String result = "";
+		for (T element : data)
+		{
+			result += element + "\n";
+		}
+		return result;
+	}
+}
