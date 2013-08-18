bd880f29b093741431bbd413afd85988836c6e6d
protocos <protocos1911@gmail.com>
2013-08-18 15:17:16 -0500

    added jars to classpath

--- xTeam/src/me/protocos/xteam/testing/GeneralTestPad.java
+++ xTeam/src/me/protocos/xteam/testing/GeneralTestPad.java
@@ -1,8 +1,6 @@
 package me.protocos.xteam.testing;
 
 import java.io.IOException;
-import me.protocos.xteam.xTeam;
-import me.protocos.xteam.util.ErrorReportUtil;
 import org.junit.Test;
 
 public class GeneralTestPad
@@ -10,7 +8,6 @@ public class GeneralTestPad
 	@Test
 	public void test() throws IOException
 	{
-		ErrorReportUtil util = new ErrorReportUtil(new xTeam());
-		util.sendErrorReport(new NullPointerException());
+
 	}
 }
