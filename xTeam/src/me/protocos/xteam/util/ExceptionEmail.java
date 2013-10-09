07c3c7370337629062118d6d54ced0e590724455
protocos <protocos1911@gmail.com>
2013-10-08 23:47:28 -0500

    Added ExceptionEmail to tracking

---
+++ xTeam/src/me/protocos/xteam/util/ExceptionEmail.java
@@ -0,0 +1,43 @@
+package me.protocos.xteam.util;
+
+public class ExceptionEmail
+{
+	private Exception exception;
+	private String subject;
+	private String body;
+
+	private ExceptionEmail()
+	{
+
+	}
+
+	public Exception getException()
+	{
+		return exception;
+	}
+
+	public String getSubject()
+	{
+		return subject;
+	}
+
+	public String getBody()
+	{
+		return body;
+	}
+	public static ExceptionEmail parse(Exception e, String pluginPackageID)
+	{
+		ExceptionEmail email = new ExceptionEmail();
+		String errorString = e.toString();
+		email.subject = errorString.substring(errorString.lastIndexOf(".") + 1) + " in project";
+		email.body = "[ERROR] " + errorString;
+		for (StackTraceElement elem : e.getStackTrace())
+		{
+			if (elem.toString().contains(pluginPackageID))
+			{
+				email.body += ("\n\t@ " + elem.toString().replaceAll(pluginPackageID, ""));
+			}
+		}
+		return email;
+	}
+}
