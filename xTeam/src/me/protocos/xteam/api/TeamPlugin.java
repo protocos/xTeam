3e8b4fe5a7fa3f31f40e96f8e1e321bb1e63db7d
protocos <protocos1911@gmail.com>
2013-11-11 21:24:19 -0600

    preliminary changes...

---
+++ xTeam/src/me/protocos/xteam/api/TeamPlugin.java
@@ -0,0 +1,52 @@
+package me.protocos.xteam.api;
+
+import java.util.List;
+import me.protocos.xteam.api.command.ICommandManager;
+import me.protocos.xteam.api.util.ILog;
+import me.protocos.xteam.core.PlayerManager;
+import me.protocos.xteam.core.ServiceManager;
+import me.protocos.xteam.core.TeamManager;
+import me.protocos.xteam.util.ConfigLoader;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public abstract class TeamPlugin extends JavaPlugin
+{
+	public TeamPlugin()
+	{
+		super();
+	}
+
+	public abstract ILog getLog();
+
+	public abstract String getVersion();
+
+	public abstract String getPluginName();
+
+	public abstract ServiceManager getServiceManager();
+
+	public abstract ICommandManager getCommandManager();
+
+	public abstract CommandExecutor getCommandExecutor();
+
+	public abstract TeamManager getTeamManager();
+
+	public abstract PlayerManager getPlayerManager();
+
+	public abstract ConfigLoader getConfigLoader();
+
+	public abstract void initFileSystem(String path);
+
+	public abstract List<Permission> getPermissions();
+
+	public abstract void registerConsoleCommands(ICommandManager manager);
+
+	public abstract void registerServerAdminCommands(ICommandManager manager);
+
+	public abstract void registerLeaderCommands(ICommandManager manager);
+
+	public abstract void registerAdminCommands(ICommandManager manager);
+
+	public abstract void registerUserCommands(ICommandManager manager);
+}
