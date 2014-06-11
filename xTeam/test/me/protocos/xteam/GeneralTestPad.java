a500b7e615cdce4461f5221d2d30a1fb3df2d907
protocos <protocos1911@gmail.com>
2014-06-11 16:48:58 -0500

    Removed eroneous code from GeneralTestPad

--- xTeam/test/me/protocos/xteam/GeneralTestPad.java
+++ xTeam/test/me/protocos/xteam/GeneralTestPad.java
@@ -1,6 +1,5 @@
 package me.protocos.xteam;
 
-import me.protocos.xteam.util.PatternBuilder;
 import org.junit.Test;
 
 public class GeneralTestPad
@@ -8,6 +7,5 @@ public class GeneralTestPad
 	@Test
 	public void ShouldBeSomething()
 	{
-		System.out.println(new PatternBuilder().anyUnlimitedOptional(new PatternBuilder().alphaNumeric().append(",")).whiteSpaceOptional().matches("world,world"));
 	}
 }
