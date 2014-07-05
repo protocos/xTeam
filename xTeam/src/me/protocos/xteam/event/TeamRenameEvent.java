fbc659ad0c0433943cb76a8e56fe09e77be69669
protocos <protocos1911@gmail.com>
2014-07-05 15:15:59 -0500

    added method to RenameEvent for getNewName()

--- xTeam/src/me/protocos/xteam/event/TeamRenameEvent.java
+++ xTeam/src/me/protocos/xteam/event/TeamRenameEvent.java
@@ -5,12 +5,19 @@ import me.protocos.xteam.entity.ITeam;
 public class TeamRenameEvent implements ITeamEvent
 {
 	private final ITeam team;
+	private final String newName;
 	private final String oldName;
 
 	public TeamRenameEvent(ITeam team, String oldName)
 	{
 		this.team = team;
+		this.newName = team.getName();
 		this.oldName = oldName;
+	}
+
+	public String getNewName()
+	{
+		return newName;
 	}
 
 	public String getOldName()
